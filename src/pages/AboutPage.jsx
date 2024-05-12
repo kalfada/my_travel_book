89cd5f98ebe56fab509f8a8b0c53371317618c98
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2024-05-12 12:48:16 +0300

    feat: add about page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About</h1>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, ex! Hic ipsum iusto et sed voluptates voluptas unde fuga magnam similique, facilis consequatur voluptatum aliquam expedita ducimus laborum amet nemo harum vel soluta porro assumenda eum, sapiente doloremque vero! Autem, dolor eius! Iusto ipsum consectetur animi ipsam adipisci facere eveniet?
+            </p>
+        </main>
+    )
+}
+
+export default AboutPage
